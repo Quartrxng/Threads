71767cc9748f5c25b5836160c438665c1da35c1b
Quartrxng <newsightminecraft@gmail.com>
2025-04-28 21:08:59 +0300

    321321312

---
+++ Peterson/ConsoleApp13/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Peterson
+{
+    using System;
+    using System.Threading;
+
+    class Program
+    {
+
+    }
+
+    public class SimplePetersonLock
+    {
+        private bool[] want = new bool[2];
+        private int turn;
+
+        public void Lock(int myId)
+        {
+            int other = 1 - myId;
+            want[myId] = true;
+            turn = other;
+
+            while (want[other] && turn == other) { }
+        }
+
+        public void Unlock(int myId)
+        {
+            want[myId] = false;
+        }
+    }
+    public class BrokenPetersonLock
+    {
+        private bool[] want = new bool[2];  
+        private int turn;                   
+
+        public void Lock(int myId)
+        {
+            int other = 1 - myId;
+            want[myId] = true;
+
+            while (want[other] && turn == other) { }
+            turn = other;
+        }
+
+        public void Unlock(int myId)
+        {
+            want[myId] = false;
+        }
+    }
+}
